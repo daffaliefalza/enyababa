a080692bb5dc974eb92c2daeaa9b4e7a8fa5adad
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2024-10-03 14:08:32 +0700

    fixing footer

--- js/script.js
+++ js/script.js
@@ -11,3 +11,46 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// scroll reveals
+ScrollReveal().reveal(".testimoni", {
+  delay: 700,
+  origin: "top",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".about__img-profile", {
+  delay: 700,
+  origin: "left",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".about__description", {
+  delay: 500,
+  origin: "right",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".skills__title", {
+  delay: 700,
+  origin: "top",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".skills-list", {
+  delay: 700,
+  origin: "bottom",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".projects__title", {
+  delay: 700,
+  origin: "top",
+  distance: "80px",
+});
+
+ScrollReveal().reveal(".projects .grid-container", {
+  delay: 700,
+  origin: "bottom",
+  distance: "80px",
+});
